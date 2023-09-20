38bced941650416742dbda655d843977f8e648ba
Mike Owino <mike.owino40@yahoo.com>
2023-09-20 20:04:00 +0000

    gmail

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,14 +1,14 @@
-import nodemailer from "nodemailer";
-import { render } from "@react-email/render";
-import { SpringSalesMail } from "@/react-email-starter/emails/spring-sales";
+import nodemailer from 'nodemailer'
+import { render } from '@react-email/render'
+import { SpringSalesMail } from '../../../react-email-starter/emails/spring-sales'
 
 export async function POST(req: Request) {
-  const { name, email } = await req.json();
+  const { name, email } = await req.json()
 
-  const emailHtml = render(SpringSalesMail({ userName: name }));
+  const emailHtml = render(SpringSalesMail({ userName: name }))
 
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    service: 'gmail',
     auth: {
       user: process.env.GMAIL_USER,
       pass: process.env.GMAIL_PASSWORD,
@@ -16,35 +16,35 @@ export async function POST(req: Request) {
     tls: {
       rejectUnauthorized: false,
     },
-  });
+  })
 
   const mailOptions = {
     from: process.env.GMAIL_USER,
     to: email,
     subject: "Spring flower sales💐 Don't miss out!",
     html: emailHtml,
-  };
+  }
 
   if (!name || !email) {
     return new Response(
-      JSON.stringify({ message: "Please submit your name and email" }),
+      JSON.stringify({ message: 'Please submit your name and email' }),
       { status: 400 }
-    );
+    )
   }
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.error(error);
+      console.error(error)
       return new Response(
-        JSON.stringify({ message: "Error: Could not send email" }),
+        JSON.stringify({ message: 'Error: Could not send email' }),
         { status: 400 }
-      );
+      )
     }
 
-    console.log("Email sent: " + info.response);
+    console.log('Email sent: ' + info.response)
     return new Response(
-      JSON.stringify({ message: "Email sent successfully" }),
+      JSON.stringify({ message: 'Email sent successfully' }),
       { status: 200 }
-    );
-  });
+    )
+  })
 }
